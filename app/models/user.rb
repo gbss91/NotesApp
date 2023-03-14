b4e1f6eadb6b8c679e6bbf24713338f239b762b4
Gabriel S <gbss91@gmail.com>
2023-03-14 18:53:30 +0000

    Fixed all vulnerabilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,16 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, presence: true, allow_blank: false
 
+  # Devise strong password - https://github.com/heartcombo/devise/wiki/How-To:-Set-up-simple-password-complexity-requirements
+  validate :password_complexity
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+
+    errors.add :password,
+               'must have at least 8 characters and include 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
   has_many :notes, dependent: :destroy
 end
